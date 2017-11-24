0998881633f05f19aa62a071dbb177d40d8e65d3
Ryabushkin Mikhail <ryabushkin.mikhail@gmail.com>
2017-11-24 18:57:32 +0300

    refactoring 2

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,10 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var pug = require('gulp-pug');
 var browserSync = require('browser-sync').create();
-
-// css
-gulp.task('css', function() {
-  return gulp.src('src/scss/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('build/css'));
-    .pipe(browserSync.stream());
-});
+var imagemin = require('gulp-imagemin');
+var newer = require('gulp-newer');
+var del = require("del");
+var plumber = require('gulp-plumber'); // отлавливает ошибки в себя
 
 // html
 gulp.task('html', function() {
@@ -19,19 +15,55 @@ gulp.task('html', function() {
     .pipe(browserSync.stream());
 });
 
+// css
+gulp.task('css', function() {
+  return gulp.src('src/scss/style.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest('build/css'))
+    .pipe(browserSync.stream());
+});
+
+// img
+gulp.task("img", function(){
+  return gulp.src('src/img/*')
+  .pipe(newer('build/img'))
+  .pipe(gulp.dest('build/img'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task("imgmin", ["img"], function(){
+  return gulp.src('src/img/*')
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest('build/img'))
+    .pipe(browserSync.stream());
+});
+
+//fonts
+gulp.task("fonts", function(){
+  return gulp.src('src/fonts/*')
+  .pipe(newer('build/fonts'))
+  .pipe(gulp.dest('build/fonts'))
+  .pipe(browserSync.stream());
+});
+
 // server
-gulp.task('server', function() {
+gulp.task('serve', function() {
   browserSync.init({
     server: "build"
   });
-  browserSync.watch('build', browserSync.reload);
-});
-
-// watch
-gulp.task('watch', function() {
   gulp.watch('src/scss/**/*.scss', ['css'])
   gulp.watch('src/pug/*.pug', ['html'])
-})
+  gulp.watch('src/img/*', ['imgmin'])
+  gulp.watch('src/fonts/*', ['fonts'])
+});
+
+gulp.task("clean", function(){
+  return del(["build/**/*.*"], {read: false});
+});
 
 // default
-gulp.task('default', [ 'html', 'css', 'watch', 'server' ]);
+gulp.task('default', [ 'clean', 'html', 'css', 'img', 'imgmin', 'fonts', 'serve' ]);
