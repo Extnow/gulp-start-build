db3cb473e53f4f9009b23a7809eaa28c152687e6
Ryabushkin Mikhail <ryabushkin.mikhail@gmail.com>
2017-11-24 13:20:30 +0300

    refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,10 @@ var browserSync = require('browser-sync').create();
 
 // css
 gulp.task('css', function() {
-  return gulp.src('src/scss/*.scss')
+  return gulp.src('src/scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('build/css'));
+    .pipe(browserSync.stream());
 });
 
 // html
@@ -15,13 +16,8 @@ gulp.task('html', function() {
   return gulp.src('src/pug/*.pug')
     .pipe(pug({ pretty: true }))
     .pipe(gulp.dest('build'))
+    .pipe(browserSync.stream());
 });
-
-// watch
-gulp.task('watch', function() {
-  gulp.watch('src/scss/*.scss', ['css'])
-  gulp.watch('src/pug/*.pug', ['html'])
-})
 
 // server
 gulp.task('server', function() {
@@ -30,6 +26,12 @@ gulp.task('server', function() {
   });
   browserSync.watch('build', browserSync.reload);
 });
+
+// watch
+gulp.task('watch', function() {
+  gulp.watch('src/scss/**/*.scss', ['css'])
+  gulp.watch('src/pug/*.pug', ['html'])
+})
 
 // default
 gulp.task('default', [ 'html', 'css', 'watch', 'server' ]);
